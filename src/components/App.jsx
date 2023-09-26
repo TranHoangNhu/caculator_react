c4bee49f1dbe737acabadc9be5aee526b9124382
Tran Hoang Nhu <tran.hoang.nhu.1997@gmail.com>
2023-09-26 23:19:51 +0700

    add logic calc in button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,12 @@ import "../scss/index.scss";
 import useDarkMode from "./Themes";
 import SoundEffect from "./SoundEffect";
 import ShortcutKeyboard from "./ShortcutKeyboard";
+import useCalc from "./Calc";
 
 export default function App() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
-
+  const { currentValue, handleClear, handleCalculate, handleButtonClick } =
+    useCalc();
   return (
     <div
       className={`container ${isDarkMode ? "dark-mode" : ""}`}
@@ -31,24 +33,38 @@ export default function App() {
           <div className="contain-caculator mx-auto col-xl-3 col-md-6 col-11">
             <div className={`caculator ${isDarkMode ? "dark-mode" : ""}`}>
               <div className="buttons">
-                <h2 id="value">123</h2>
-                <span id="clear">CLEAR</span>
-                <span>/</span>
-                <span>*</span>
-                <span>7</span>
-                <span>8</span>
-                <span>9</span>
-                <span>-</span>
-                <span>4</span>
-                <span>5</span>
-                <span>6</span>
-                <span style={{ gridRow: "span 2" }}>+</span>
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
-                <span style={{ gridColumn: "span 2" }}>0</span>
-                <span>.</span>
-                <span id="equal">=</span>
+                <h2 id="value">{currentValue}</h2>
+                <span id="clear" onClick={handleClear}>
+                  CLEAR
+                </span>
+                <span onClick={() => handleButtonClick("/")}>/</span>
+                <span onClick={() => handleButtonClick("*")}>*</span>
+                <span onClick={() => handleButtonClick("7")}>7</span>
+                <span onClick={() => handleButtonClick("8")}>8</span>
+                <span onClick={() => handleButtonClick("9")}>9</span>
+                <span onClick={() => handleButtonClick("-")}>-</span>
+                <span onClick={() => handleButtonClick("4")}>4</span>
+                <span onClick={() => handleButtonClick("5")}>5</span>
+                <span onClick={() => handleButtonClick("6")}>6</span>
+                <span
+                  style={{ gridRow: "span 2" }}
+                  onClick={() => handleButtonClick("+")}
+                >
+                  +
+                </span>
+                <span onClick={() => handleButtonClick("1")}>1</span>
+                <span onClick={() => handleButtonClick("2")}>2</span>
+                <span onClick={() => handleButtonClick("3")}>3</span>
+                <span
+                  style={{ gridColumn: "span 2" }}
+                  onClick={() => handleButtonClick("0")}
+                >
+                  0
+                </span>
+                <span onClick={() => handleButtonClick(".")}>.</span>
+                <span id="equal" onClick={handleCalculate}>
+                  =
+                </span>
               </div>
             </div>
           </div>
---
+++ src/components/Calc.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Calc() {
+  // Khởi tạo state để lưu giá trị hiện tại của máy tính
+  const [currentValue, setCurrentValue] = useState("0");
+
+  // Hàm xử lý khi nhấn các phím số hoặc phép tính
+  const handleButtonClick = (value) => {
+    // Kiểm tra nếu giá trị hiện tại là "0" hoặc kết quả trước đó là kết quả cuối cùng
+    if (currentValue === "0" || currentValue === "Result") {
+      // Đặt giá trị mới là giá trị của phím được nhấn
+      setCurrentValue(value);
+    } else {
+      // Nếu không, thêm giá trị của phím được nhấn vào giá trị hiện tại
+      setCurrentValue(currentValue + value);
+    }
+  };
+
+  // Hàm xử lý khi nhấn nút "=" để tính toán kết quả
+  const handleCalculate = () => {
+    try {
+      // Sử dụng hàm eval để tính toán biểu thức trong giá trị hiện tại
+      const result = eval(currentValue);
+      // Đặt giá trị mới là kết quả tính toán
+      setCurrentValue(result.toFixed(2).toString());
+    } catch (error) {
+      // Xử lý lỗi nếu có
+      setCurrentValue("Error");
+    }
+  };
+
+  // Hàm xử lý khi nhấn nút "Clear" để xóa giá trị hiện tại
+  const handleClear = () => {
+    setCurrentValue("0");
+  };
+
+  return { currentValue, handleClear, handleCalculate, handleButtonClick };
+}
