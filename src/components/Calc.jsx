fd8ce458a5008a324f130525ea8a6c913e2b2d6a
Tran Hoang Nhu <tran.hoang.nhu.1997@gmail.com>
2023-09-27 19:44:50 +0700

    fix đecimal logic result

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -20,9 +20,18 @@ export default function Calc() {
   const handleCalculate = () => {
     try {
       // Sử dụng hàm eval để tính toán biểu thức trong giá trị hiện tại
-      const result = eval(currentValue);
+      let result = eval(currentValue);
+      let numberAsString = result.toString();
+      let decimalIndex = numberAsString.indexOf(".");
+      if (decimalIndex !== -1) {
+        // Lấy độ dài của phần sau dấu chấm thập phân bằng cách trừ đi vị trí của dấu chấm từ độ dài tổng cộng của chuỗi
+        const lengthAfterDecimal = numberAsString.length - decimalIndex - 1;
+        if (parseInt(lengthAfterDecimal) > 2) {
+          result = result.toFixed(2);
+        }
+      }
       // Đặt giá trị mới là kết quả tính toán
-      setCurrentValue(result.toFixed(2).toString());
+      setCurrentValue(result.toString());
     } catch (error) {
       // Xử lý lỗi nếu có
       setCurrentValue("Error");
